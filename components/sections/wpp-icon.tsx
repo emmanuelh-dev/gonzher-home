3961cd1695967475caa3e0ca252da1d0bd214e79
Emmanuel Hernandez <e805177@gmail.com>
2023-09-27 17:41:34 -0600

    Update wpp-icon.tsx

--- components/sections/wpp-icon.tsx
+++ components/sections/wpp-icon.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BsWhatsapp } from "react-icons/bs";
 const WhatsAppIcon = () => {
   return (
-    <a href="https://api.whatsapp.com/send?phone=528117772171&text=" className="fixed bottom-10 right-10 z-[100]">
+    <a href="https://api.whatsapp.com/send?phone=4684513&text=" className="fixed bottom-10 right-10 z-[100]">
       <BsWhatsapp className="text-green-500 text-5xl lg:text-6xl" />
     </a>
   );
