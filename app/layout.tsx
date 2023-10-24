4aecd9ea50d68d4cb1804bfeb47e0ecab380b331
Emmanuel Hernandez <e805177@gmail.com>
2023-10-24 17:40:30 -0600

    [update]:icons were added

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <link rel="icon" href="/logo.png" />
       <body className={inter.className}>
         <Header/>
         {children}
