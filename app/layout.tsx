2387548f133d01238d83d195c7fcc309c69d530f
Emmanuel Hernandez <e805177@gmail.com>
2023-11-01 11:03:06 -0600

    fix: resize icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <link rel="icon" href="/logo.png" />
       <body className={inter.className}>
         <Header />
         {children}
