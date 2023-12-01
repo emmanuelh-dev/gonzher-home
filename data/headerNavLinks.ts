ffdc5ca345e582e03cd576ddbc86087a7547d91e
emmanuelh-dev <86961961+emmanuelh-dev@users.noreply.github.com>
2023-12-01 09:14:37 -0600

    Update headerNavLinks.ts

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,5 +2,7 @@ export const headerNavLinks = [
   { href: "#acerca-de", title: "Nosotros" },
   { href: "#precios", title: "Precios" },
   { href: "#contactanos", title: "Contáctanos" },
-  { href: "https://system.gonzher.com/login", title: "Iniciar Sesión" },
+  { href: "https://system.gonzher.com/login", title: "Systema Gonzher" },
+  { href: "https://api.gonzher.com/login", title: "GPS Gonzher" },
+
 ];
