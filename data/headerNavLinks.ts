8b95318946268132d3d6360bb7ee957cd6567af2
Emmanuel Hernandez <e805177@gmail.com>
2024-01-04 10:53:40 -0600

    Update headerNavLinks.ts

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,7 +1,7 @@
 export const headerNavLinks = [
-  { href: "#acerca-de", title: "Nosotros" },
-  { href: "#precios", title: "Precios" },
-  { href: "#contactanos", title: "Contáctanos" },
+  { href: "/#acerca-de", title: "Nosotros" },
+  { href: "/#precios", title: "Precios" },
+  { href: "/#contactanos", title: "Contáctanos" },
   { href: "https://system.gonzher.com/login", title: "Systema Gonzher" },
   { href: "https://api.gonzher.com/login", title: "GPS Gonzher" },
 ];
