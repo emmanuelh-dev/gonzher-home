c062d04a3c7d7b0b607444251fa2e22c428ee263
emmanuelh-dev <e805177@gmail.com>
2024-06-04 12:05:12 -0600

    update pricing.tsx and ServiceCard.tsx

--- app/sections/pricing.tsx
+++ app/sections/pricing.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { BiCheck } from "react-icons/bi";
+import React from 'react';
+import { BiCheck } from 'react-icons/bi';
 interface Producto {
   id: number;
   name: string;
@@ -13,37 +13,37 @@ const Pricing = async () => {
     const response = await fetch(
       `${process.env.NEXT_PUBLIC_API_URL}/productos`,
       {
-        method: "GET",
+        method: 'GET',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
-      },
+      }
     );
 
     if (!response.ok) {
-      throw new Error("Error al obtener los productos");
+      throw new Error('Error al obtener los productos');
     }
 
     const data = await response.json();
     productos = data.productos;
   } catch (error) {
-    console.error("Error al obtener los productos:", error);
+    console.error('Error al obtener los productos:', error);
   }
 
   return (
     <div>
       <section
-        id="precios"
-        className=" bg-white pt-20 lg:pt-[120px] pb-12 lg:pb-[90px] relative z-20 overflow-hidden "
+        id='precios'
+        className=' bg-white pt-20 lg:pt-[120px] pb-12 lg:pb-[90px] relative z-20 overflow-hidden pattern'
       >
-        <div className="container mx-auto">
-          <div className="flex flex-wrap -mx-4">
-            <div className="w-full px-4">
-              <div className="text-center mx-auto mb-[60px] lg:mb-20 max-w-[800px]">
-                <h2 className="animate-fade-up font-bold text-3xl sm:text-4xl md:text-[40px] text-dark mb-4 animate-fade-up">
+        <div className='container mx-auto'>
+          <div className='flex flex-wrap -mx-4'>
+            <div className='w-full px-4'>
+              <div className='text-center mx-auto mb-[60px] lg:mb-20 max-w-[800px]'>
+                <h2 className='animate-fade-up font-bold text-3xl sm:text-4xl md:text-[40px] text-dark mb-4 animate-fade-up'>
                   Conoce nuestros planes
                 </h2>
-                <p className=" text-lg sm:text-xl leading-relaxed sm:leading-relaxed text-body-color ">
+                <p className=' text-lg sm:text-xl leading-relaxed sm:leading-relaxed text-body-color '>
                   Si tiene alguna pregunta o necesita más información sobre
                   nuestros costos unitarios, no dude en ponerse en contáctanos
                   con nuestro equipo de atención al cliente. Estamos aquí para
@@ -53,41 +53,70 @@ const Pricing = async () => {
               </div>
             </div>
           </div>
-          <div className="md:flex gap-6 items-center justify-center flex-wrap">
+          <div className='md:flex gap-6 items-center justify-center flex-wrap'>
             {productos ? (
               productos.map((producto) => (
-                <div className="w-full max-w-sm animate-fade-up" key={producto.id}>
+                <div
+                  className='w-full max-w-sm animate-fade-up'
+                  key={producto.id}
+                >
                   <div
-                    className=" bg-white rounded-xl relative z-10 overflow-hidden border border-primary border-opacity-20 shadow-pricing py-10 px-8 sm:p-12 lg:py-10 lg:px-6 xl:p-12 mb-10 text-center wow fadeInUp "
-                    data-wow-delay=".15s "
+                    className='shadow-xl bg-white rounded-xl relative z-10 overflow-hidden py-10 px-8 sm:p-12 lg:py-10 lg:px-6 xl:p-12 mb-10 text-center wow fadeInUp '
+                    data-wow-delay='.15s '
                   >
-                    <span className="text-dark font-medium text-base uppercase block mb-2">
+                    <span className='text-dark font-medium text-base uppercase block mb-2'>
                       {producto.name}
                     </span>
-                    <h2 className="font-semibold text-primary text-[20px] line-through">
+                    <h2 className='font-semibold text-primary text-[20px] line-through'>
                       {+producto.price + 100}/Año
                     </h2>
-                    <h3 className="font-semibold text-red-400 mb-9 text-[28px]">
+                    <h3 className='font-semibold text-red-400 mb-9 text-[28px]'>
                       {producto.price}/Año
                     </h3>
-                    <div className="mb-10">{producto.description}</div>
-                    <div className="w-full">
+                    <div className='mb-10'>{producto.description}</div>
+                    <div className='w-full'>
                       <a
-                        href=""
-                        className=" inline-block text-base font-medium text-primary bg-transparent border border-[#D4DEFF] rounded-full text-center py-4 px-11 hover:text-white hover:bg-primary hover:border-primary transition duration-300 ease-in-out "
+                        href=''
+                        className=' inline-block text-base font-medium text-primary bg-transparent border border-[#D4DEFF] rounded-full text-center py-4 px-11 hover:text-white hover:bg-primary hover:border-primary transition duration-300 ease-in-out '
                       >
                         Comprar Ahora
                       </a>
                     </div>
-                    <span className=" absolute left-0 bottom-0 z-[-1] w-14 h-14 rounded-tr-full block bg-primary "></span>
+                    <span className=' absolute left-0 bottom-0 z-[-1] w-14 h-14 rounded-tr-full block bg-primary '></span>
                   </div>
                 </div>
               ))
             ) : (
-              <div className="flex items-center justify-center text-center w-full">
+              <div className='flex items-center justify-center text-center w-full'>
                 Proximamente...
               </div>
             )}
+            <div className='w-full max-w-sm animate-fade-up'>
+              <div
+                className='shadow-xl bg-white rounded-xl relative z-10 overflow-hidden py-10 px-8 sm:p-12 lg:py-10 lg:px-6 xl:p-12 mb-10 text-center wow fadeInUp'
+                data-wow-delay='.15s'
+              >
+                <span className='text-dark font-medium text-base uppercase block mb-2'>
+                  Pesonaliza tu plan
+                </span>
+                <h2 className='font-semibold text-primary text-[20px] mb-4'>
+                  Enterprise
+                </h2>
+                <div className='mb-10'>
+                  Contáctanos para personalizar tu plan de acuerdo a tus
+                  necesidades.
+                </div>
+                <div className='w-full'>
+                  <a
+                    href=''
+                    className='inline-block text-base font-medium text-primary bg-transparent rounded-full text-center py-4 px-11 hover:text-white hover:bg-primary hover:border-primary transition duration-300 ease-in-out'
+                  >
+                    Contacto ventas
+                  </a>
+                </div>
+                <span className='absolute left-0 bottom-0 z-[-1] w-14 h-14 rounded-tr-full block bg-primary'></span>
+              </div>
+            </div>
           </div>
         </div>
       </section>
--- components/ui/ServiceCard.tsx
+++ components/ui/ServiceCard.tsx
@@ -13,7 +13,7 @@ export default function ServiceCard({ service, children }: Props) {
   const [open, setOpen] = useState(false);
   const { titulo, descripcion, icon } = service;
   return (
-    <div className="mb-12 p-8 bg-primary rounded-2xl text-white flex flex-col items-center justify-center max-w-[20rem] mx-auto hover:-translate-y-5 transition-all duration-500">
+    <div className="mb-12 p-8 bg-primary rounded-2xl text-white flex flex-col items-center justify-center max-w-[20rem] mx-auto hover:-translate-y-5 transition-all duration-500 shadow-lg">
       {icon}
       <h4 className="font-bold text-2xl text-dark mb-3  text-center">{titulo}</h4>
       <p
