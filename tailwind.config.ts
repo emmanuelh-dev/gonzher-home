9d57c29bdc5b9fec03b3a76f9ac8cf5f4512b4b2
Emmanuel Hernandez <e805177@gmail.com>
2024-06-11 00:51:36 -0600

    fix animation in featured questions

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -59,23 +59,29 @@ const config: Config = {
         'switch-1': '0px 0px 5px rgba(0, 0, 0, 0.15)',
         testimonial: '0px 60px 120px -20px #EBEFFD',
       },
-    },
-  },
-  variants: {
-    extend: {
+      animation: {
+        marquee: "marquee var(--duration) linear infinite",
+        "marquee-vertical": "marquee-vertical var(--duration) linear infinite",
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
       keyframes: {
+        marquee: {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(calc(-100% - var(--gap)))" },
+        },
+        "marquee-vertical": {
+          from: { transform: "translateY(0)" },
+          to: { transform: "translateY(calc(-100% - var(--gap)))" },
+        },
         'accordion-down': {
-          from: { height: 0 },
+          from: { height: '0' },
           to: { height: 'var(--radix-accordion-content-height)' },
         },
         'accordion-up': {
           from: { height: 'var(--radix-accordion-content-height)' },
-          to: { height: 0 },
+          to: { height: '0' },
         },
-      },
-      animation: {
-        'accordion-down': 'accordion-down 0.2s ease-out',
-        'accordion-up': 'accordion-up 0.2s ease-out',
       },
     },
   },
