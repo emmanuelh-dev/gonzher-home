b54653ab6325438bb024828aa1527d8c5e42f06e
Emmanuel Hernandez <e805177@gmail.com>
2024-06-11 01:49:02 -0600

    fix people love us sectionm

--- app/sections/NosAman.tsx
+++ app/sections/NosAman.tsx
@@ -100,8 +100,8 @@ const NosAman = () => {
           <ReviewCard key={review.username} {...review} />
         ))}
       </Marquee>
-      <div className='pointer-events-none absolute inset-y-0 left-0 w-1/3 bg-gradient-to-r from-white dark:from-background'></div>
-      <div className='pointer-events-none absolute inset-y-0 right-0 w-1/3 bg-gradient-to-l from-white dark:from-background'></div>
+      <div className='pointer-events-none absolute inset-y-0 left-0 w-1/3 lg:bg-gradient-to-r from-white'></div>
+      <div className='pointer-events-none absolute inset-y-0 right-0 w-1/3 lg:bg-gradient-to-l from-white'></div>
     </section>
   );
 };
